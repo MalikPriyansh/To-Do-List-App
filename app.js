2242e22d5db985e3ef405946416190890394561c
Priyansh Malik <priyanshmalik332@gmail.com>
2022-05-25 00:20:49 +0530

    Some updation

--- app.js
+++ app.js
@@ -1,3 +1,43 @@
+//const http = require("http");
+/*const express = require("express");
+
+const app = express();*/
+const mongoose = require("mongoose");
+
+mongoose.connect("mongodb://localhost:27017/todolistdb");
+
+const structure = {
+    name : String
+};
+
+const Item = mongoose.model("item",structure);
+
+const gym = new Item({
+    name : "Gym"
+});
+
+const eat = new Item({
+    name : "Eat"
+});
+
+const sleep = new Item({
+    name : "Sleep"
+});
+
+const defaultitems = [gym,eat,sleep];
+
+Item.insertMany(defaultitems,function(err){
+    if(err)
+    {
+        console.log(err);
+    }
+    else
+    {
+        console.log("Successfully saved the data in the database");
+    }
+});
+
+
 function callback(){
     let element = document.getElementById("textbar").value;
     if(element=="")
@@ -21,13 +61,18 @@ function callback(){
     document.getElementById("textbar").value="";
 }
 
+if(typeof document !== "undefined")
+{
+    let btn = document.getElementById("ul");
+    btn.addEventListener("click",function(e){
+        let master = document.getElementById("ul");
+        let li = e.target.parentNode;
+        master.removeChild(li);
+    });
+}
 
-
-let btn = document.getElementById("ul");
-btn.addEventListener("click",function(e){
-    let master = document.getElementById("ul");
-    let li = e.target.parentNode;
-    master.removeChild(li);
-});
+/*app.listen(3000,function(){
+    console.log("Server has been started");
+});*/
 
 
